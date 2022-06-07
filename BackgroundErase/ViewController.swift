8edd0fdaae47f1dfb4ee27f0291d0bf073c9ff06
Walter Tyree <walter@tyreeapps.com>
2022-06-07 07:57:23 -0400

    Initial Commit

---
+++ BackgroundErase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BackgroundErase
+//
+//  Created by Walter Tyree on 6/7/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
